9fc57350e27b23f0bc914dc4d4cb06e8ee8dc249
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-06-16 21:13:34 -0500

    blaze layout files

--- app/routes.php
+++ app/routes.php
@@ -13,5 +13,9 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+    return View::make('layout');
+});
+
+Route::get('users', function() {
+  return View::make('users');
 });
---
+++ app/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+        <meta http-equiv="X-USA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<title>Laravel PHP Framework</title>
+</head>
+<body>
+    <header class="header-homepage">
+        <h1>Welcome</h1>
+    </header>
+    @yield('content')
+</body>
+</html>
