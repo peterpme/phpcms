9bd138387eb0e2db88d4ed159678387c3689a069
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-06-17 20:50:10 -0500

    logging in functionality, block dashboard from unathorized users

--- app/models/User.php
+++ app/models/User.php
@@ -31,6 +31,4 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $hidden = array('password', 'remember_token');
         
-        public static $rules = array()
-
 }
