9a4fde64372b853a8ae67d1139c122aa818f8257
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-06-19 19:37:47 -0500

    pages creation

---
+++ app/controllers/PagesController.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class PagesController extends BaseController {
+    
+    protected $layout = "layouts.main";
+    
+    public function __construct() {
+        $this->beforeFilter('csrf', array('on' => 'post'));
+        $this->beforeFilter('auth', array('only' => array('getDashboard')));
+    }
+}
